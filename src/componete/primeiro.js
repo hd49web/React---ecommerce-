792021a6e9b05fb43f0cdfa9771591350beea5ef
hd49web <hd49web@gmail.com>
2022-08-07 17:31:24 -0300

    entregar trabalho

---
+++ src/componete/primeiro.js
@@ -0,0 +1,231 @@
+
+function Primeiro(){
+return(
+    <div>
+ 
+<div class="container">
+<div class="card">
+<div class="imgBx">
+	<img src="img/1.jpg"/>
+	<ul class="action">
+	<li><i class="fa fa-heart" aria-hidden="true"></i>
+	<span>Adicionar à lista de desejos</span></li>
+	<li><i class="fa fa-shopping-cart" aria-hidden="true"></i><span>Adicionar ao carrinho</span></li>
+	<li><i class="fa fa-eye" aria-hidden="true"></i><span>Ver detalhes</span></li>
+	</ul>
+</div>
+<div class="content">
+	<div class="productName">
+		<h3>Vestido de linha de Rayon</h3>
+	</div>
+	<div class="price_rating">
+		<h2>R$ 13.45</h2>
+<div class="rating">
+	<i class="fa fa-star" aria-hidden="true"></i>
+	<i class="fa fa-star" aria-hidden="true"></i>
+	<i class="fa fa-star" aria-hidden="true"></i>
+	<i class="fa fa-star grey" aria-hidden="true"></i>
+	<i class="fa fa-star grey" aria-hidden="true"></i>
+</div>
+</div>
+</div>
+</div>
+
+<div class="card">
+<div class="imgBx">
+	<img src="img/2.jpg"/>
+	<ul class="action">
+	<li><i class="fa fa-heart" aria-hidden="true"></i>
+	<span>Adicionar à lista de desejos</span></li>
+	<li><i class="fa fa-shopping-cart" aria-hidden="true"></i><span>Adicionar ao carrinho</span></li>
+	<li><i class="fa fa-eye" aria-hidden="true"></i><span>Ver detalhes</span></li>
+	</ul>
+</div>
+<div class="content">
+	<div class="productName">
+		<h3>Vestido de linha de Rayon</h3>
+	</div>
+	<div class="price_rating">
+		<h2>R$ 13.45</h2>
+<div class="rating">
+	<i class="fa fa-star" aria-hidden="true"></i>
+	<i class="fa fa-star" aria-hidden="true"></i>
+	<i class="fa fa-star grey" aria-hidden="true"></i>
+	<i class="fa fa-star grey" aria-hidden="true"></i>
+	<i class="fa fa-star grey" aria-hidden="true"></i>
+</div>
+</div>
+</div>
+</div>
+
+<div class="card">
+<div class="imgBx">
+	<img src="img/3.jpg"/>
+	<ul class="action">
+	<li><i class="fa fa-heart" aria-hidden="true"></i>
+	<span>Adicionar à lista de desejos</span></li>
+	<li><i class="fa fa-shopping-cart" aria-hidden="true"></i><span>Adicionar ao carrinho</span></li>
+	<li><i class="fa fa-eye" aria-hidden="true"></i><span>Ver detalhes</span></li>
+	</ul>
+</div>
+<div class="content">
+	<div class="productName">
+		<h3>Vestido de linha de Rayon</h3>
+	</div>
+	<div class="price_rating">
+		<h2>R$ 13.45</h2>
+<div class="rating">
+	<i class="fa fa-star" aria-hidden="true"></i>
+	<i class="fa fa-star" aria-hidden="true"></i>
+	<i class="fa fa-star" aria-hidden="true"></i>
+	<i class="fa fa-star grey" aria-hidden="true"></i>
+	<i class="fa fa-star grey" aria-hidden="true"></i>
+</div>
+</div>
+</div>
+</div>
+
+<div class="card">
+<div class="imgBx">
+	<img src="img/4.jpg"/>
+	<ul class="action">
+	<li><i class="fa fa-heart" aria-hidden="true"></i>
+	<span>Adicionar à lista de desejos</span></li>
+	<li><i class="fa fa-shopping-cart" aria-hidden="true"></i><span>Adicionar ao carrinho</span></li>
+	<li><i class="fa fa-eye" aria-hidden="true"></i><span>Ver detalhes</span></li>
+	</ul>
+</div>
+<div class="content">
+	<div class="productName">
+		<h3>Vestido de linha de Rayon</h3>
+	</div>
+	<div class="price_rating">
+		<h2>R$ 13.45</h2>
+<div class="rating">
+	<i class="fa fa-star" aria-hidden="true"></i>
+	<i class="fa fa-star" aria-hidden="true"></i>
+	<i class="fa fa-star grey" aria-hidden="true"></i>
+	<i class="fa fa-star grey" aria-hidden="true"></i>
+	<i class="fa fa-star grey" aria-hidden="true"></i>
+</div>
+</div>
+</div>
+</div>
+
+<div class="card">
+<div class="imgBx">
+	<img src="img/5.jpg"/>
+	<ul class="action">
+	<li><i class="fa fa-heart" aria-hidden="true"></i>
+	<span>Adicionar à lista de desejos</span></li>
+	<li><i class="fa fa-shopping-cart" aria-hidden="true"></i><span>Adicionar ao carrinho</span></li>
+	<li><i class="fa fa-eye" aria-hidden="true"></i><span>Ver detalhes</span></li>
+	</ul>
+</div>
+<div class="content">
+	<div class="productName">
+		<h3>Vestido de linha de Rayon</h3>
+	</div>
+	<div class="price_rating">
+		<h2>R$ 13.45</h2>
+<div class="rating">
+	<i class="fa fa-star" aria-hidden="true"></i>
+	<i class="fa fa-star" aria-hidden="true"></i>
+	<i class="fa fa-star" aria-hidden="true"></i>
+	<i class="fa fa-star" aria-hidden="true"></i>
+	<i class="fa fa-star grey" aria-hidden="true"></i>
+</div>
+</div>
+</div>
+</div>
+
+<div class="card">
+<div class="imgBx">
+	<img src="img/6.jpg"/>
+	<ul class="action">
+	<li><i class="fa fa-heart" aria-hidden="true"></i>
+	<span>Adicionar à lista de desejos</span></li>
+	<li><i class="fa fa-shopping-cart" aria-hidden="true"></i><span>Adicionar ao carrinho</span></li>
+	<li><i class="fa fa-eye" aria-hidden="true"></i><span>Ver detalhes</span></li>
+	</ul>
+</div>
+<div class="content">
+	<div class="productName">
+		<h3>Vestido de linha de Rayon</h3>
+	</div>
+	<div class="price_rating">
+		<h2>R$ 13.45</h2>
+<div class="rating">
+	<i class="fa fa-star" aria-hidden="true"></i>
+	<i class="fa fa-star" aria-hidden="true"></i>
+	<i class="fa fa-star" aria-hidden="true"></i>
+	<i class="fa fa-star grey" aria-hidden="true"></i>
+	<i class="fa fa-star grey" aria-hidden="true"></i>
+</div>
+</div>
+</div>
+</div>
+
+<div class="card">
+<div class="imgBx">
+	<img src="img/7.jpg"/>
+	<ul class="action">
+	<li><i class="fa fa-heart" aria-hidden="true"></i>
+	<span>Adicionar à lista de desejos</span></li>
+	<li><i class="fa fa-shopping-cart" aria-hidden="true"></i><span>Adicionar ao carrinho</span></li>
+	<li><i class="fa fa-eye" aria-hidden="true"></i><span>Ver detalhes</span></li>
+	</ul>
+</div>
+<div class="content">
+	<div class="productName">
+		<h3>Vestido de linha de Rayon</h3>
+	</div>
+	<div class="price_rating">
+		<h2>R$ 13.45</h2>
+<div class="rating">
+	<i class="fa fa-star" aria-hidden="true"></i>
+	<i class="fa fa-star" aria-hidden="true"></i>
+	<i class="fa fa-star" aria-hidden="true"></i>
+	<i class="fa fa-star grey" aria-hidden="true"></i>
+	<i class="fa fa-star grey" aria-hidden="true"></i>
+</div>
+</div>
+</div>
+</div>
+
+<div class="card">
+<div class="imgBx">
+	<img src="img/8.jpg"/>
+	<ul class="action">
+	<li><i class="fa fa-heart" aria-hidden="true"></i>
+	<span>Adicionar à lista de desejos</span></li>
+	<li><i class="fa fa-shopping-cart" aria-hidden="true"></i><span>Adicionar ao carrinho</span></li>
+	<li><i class="fa fa-eye" aria-hidden="true"></i><span>Ver detalhes</span></li>
+	</ul>
+</div>
+<div class="content">
+	<div class="productName">
+		<h3>Vestido de linha de Rayon</h3>
+	</div>
+	<div class="price_rating">
+		<h2>R$ 13.45</h2>
+<div class="rating">
+	<i class="fa fa-star" aria-hidden="true"></i>
+	<i class="fa fa-star" aria-hidden="true"></i>
+	<i class="fa fa-star" aria-hidden="true"></i>
+	<i class="fa fa-star " aria-hidden="true"></i>
+	<i class="fa fa-star grey" aria-hidden="true"></i>
+</div>
+</div>
+</div>
+</div>
+
+
+
+</div>
+
+</div>
+)
+}
+
+export default Primeiro
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Primeiro from './componete/primeiro' 
+
+
+ReactDOM.render(
+  
+    <div> 
+    <Primeiro></Primeiro>
+    
+ </div>,
+    document.getElementById('root')
+     
+)
